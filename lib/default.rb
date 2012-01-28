12f802626b42cb1a81a5f36a23b3305aeb515762
John Nishinaga <jingoro@casa-z.org>
2012-01-28 17:06:43 -0500

    add workaround for stack level too deep error

--- lib/default.rb
+++ lib/default.rb
@@ -1,11 +1,18 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
-require 'compass'
+# Don't reload this file for nanoc autocompile. Otherwise, nanoc will eventually
+# throw an "ERROR SystemStackError: stack level too deep" exception.
+unless defined? LOADED_DEFAULT_CONFIG
 
-Compass.add_project_configuration File.expand_path('../../compass-config.rb', __FILE__)
+  LOADED_DEFAULT_CONFIG = true
 
-# include common helpers
-include Nanoc3::Helpers::HTMLEscape
-include Nanoc3::Helpers::LinkTo
-include Nanoc3::Helpers::Rendering
+  require 'compass'
+  Compass.add_project_configuration File.expand_path('../../compass-config.rb', __FILE__)
+
+  # include common helpers
+  include Nanoc3::Helpers::HTMLEscape
+  include Nanoc3::Helpers::LinkTo
+  include Nanoc3::Helpers::Rendering
+
+end
