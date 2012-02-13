e843d1b0f10f35f594b12a1ff325e2896f5a733a
John Nishinaga <jingoro@casa-z.org>
2012-02-12 23:39:31 -0800

    update update

--- .update.rb
+++ .update.rb
@@ -5,35 +5,26 @@ require 'open-uri'
 BASE_DIR = File.dirname(__FILE__)
 BASE_URL = 'https://raw.github.com/h5bp/html5-boilerplate/master'
 
-def update_file(local_path, url)
-  puts "Updating #{local_path}"
-  open(local_path, 'wb') do |file|
-    file << open(url).read
+{
+  '404.html' => 'content/404.html',
+  'apple-touch-icon-114x114-precomposed.png' => 'content/apple-touch-icon-114x114-precomposed.png',
+  'apple-touch-icon-57x57-precomposed.png' => 'content/apple-touch-icon-57x57-precomposed.png',
+  'apple-touch-icon-72x72-precomposed.png' => 'content/apple-touch-icon-72x72-precomposed.png',
+  'apple-touch-icon-precomposed.png' => 'content/apple-touch-icon-precomposed.png',
+  'apple-touch-icon.png' => 'content/apple-touch-icon.png',
+  'crossdomain.xml' => 'content/crossdomain.xml',
+  'favicon.ico' => 'content/favicon.ico',
+  'humans.txt' => 'content/humans.txt',
+  'robots.txt' => 'content/robots.txt',
+  'js/libs/jquery-1.7.1.js' => 'content/js/libs/jquery.js',
+  'js/libs/modernizr-2.5.2.min.js' => 'content/js/libs/modernizr.js',
+  'js/plugins.js' => 'content/js/_plugins.js',
+  'js/script.js' => 'content/js/_script.js',
+  '.htaccess' => 'h5bp.htaccess',
+  '.gitignore' => 'h5bp.gitignore',
+}.each do |repo_file, local_file|
+  puts "Updating #{local_file}"
+  open("#{BASE_DIR}/#{local_file}", 'wb') do |file|
+    file << open("#{BASE_URL}/#{repo_file}").read
   end
-end
-
-%w(
-  404.html
-  apple-touch-icon-114x114-precomposed.png
-  apple-touch-icon-57x57-precomposed.png
-  apple-touch-icon-72x72-precomposed.png
-  apple-touch-icon-precomposed.png
-  apple-touch-icon.png
-  crossdomain.xml
-  favicon.ico
-  humans.txt
-  robots.txt
-  js/libs/jquery-1.7.1.js
-  js/libs/modernizr-2.5.2.min.js
-  js/plugins.js
-  js/script.js
-).each do |file|
-  update_file "#{BASE_DIR}/content/#{file}", "#{BASE_URL}/#{file}"
-end
-
-%w(
-  .htaccess
-  .gitignore
-).each do |file|
-  update_file "#{BASE_DIR}/h5bp#{file}", "#{BASE_URL}/#{file}"
 end
